069ce5e906d26eafd6ec6eefe099b5249d2a23ec
letItCurl <rolandlopez.developer@gmail.com>
2020-09-17 15:07:50 +0200

    events emitters in services

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+//@Injectable({
+//  providedIn: 'root'
+//})
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  statusUpdated = new EventEmitter<string>();
+
+  addAccount(name: string, status: string){
+    this.accounts.push({name: name, status: status})
+  }
+
+  updateStatus(id: number, newStatus: string){
+    this.accounts[id].status = newStatus
+  }
+}
